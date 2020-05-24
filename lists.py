03c722f72663a3600062ac69ba79ae8eb32d8083
lipewinning <felipealvesdesousa@gmail.com>
2020-05-24 16:23:44 +0100

    lists

---
+++ lists.py
@@ -0,0 +1,19 @@
+my_list = [1, 2, 3]
+my_list2 = [1, "felipe", 33]
+len(my_list)
+print(my_list2[2])
+
+concat_list = my_list + my_list2;
+print(concat_list)
+my_list[2] = 35;
+print(my_list)
+
+my_list.append(43);
+print(my_list)
+print( my_list.pop()) #remove the last element
+print(my_list)
+print( my_list2.pop(2))
+print(my_list2)
+list_letters = ['f', 'e', 'a', 'w', 'i']
+list_letters.sort()
+print(list_letters)
