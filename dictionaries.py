98c4b66496c102e931525feba62eec9b06e5a3de
lipewinning <felipealvesdesousa@gmail.com>
2020-05-24 16:59:35 +0100

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+squads = {'a':'brazil', 'b':'holand', 'c':'Denamark', 'd':'Mexico'}
+print( squads['b'])
+prices_lookup = {'meat': 3.67, 'mushroons':0.79, 'peanutButther': 2.78}
+prices_lookup['tomatoes'] = 1.22
+print( prices_lookup)
+prices_lookup['meat'] = 2.99;
+print(prices_lookup)
+prices_lookup.keys(); #it returns all keys
+prices_lookup.values(); #it returns all values
+prices_lookup.items();
